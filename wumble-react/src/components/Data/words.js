428e9e46bbac2b7f756f16ac822c631bdf2a6775
flaycock <freddielaycock97@hotmail.co.uk>
2022-06-29 15:35:04 +0100

    Adding in positioning checks and list of random 4 letter words

---
+++ wumble-react/src/components/Data/words.js
@@ -0,0 +1,21 @@
+const words = [
+	'HELL',
+	'GOOD',
+	'ONYX',
+	'MILE',
+	'HILL',
+	'COLD',
+	'BONY',
+	'WASH',
+	'PURE',
+	'DUET',
+	'NIBS',
+	'OPEN',
+	'FOOD',
+	'READ',
+	'LOAM',
+	'VIAL',
+	'KILN'
+];
+
+export default words;
--- wumble-react/src/components/Game.jsx
+++ wumble-react/src/components/Game.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './Game.css';
+import words from './Data/words.js';
 
 const Game = () => {
 	const [input, setInput] = useState('');
@@ -7,13 +8,14 @@ const Game = () => {
 	const [attempts, setAttempts] = useState(0);
 	const [success, setSuccess] = useState(false);
 	const [failure, setFailure] = useState(false);
-	const wordAns = 'MOOD';	
+	const [wordAns, setWordAns] = useState('');
+	const [inputDisabled, setInputDisabled] = useState('');
+	const ref = useRef(null);
+	if (wordAns === '') { setWordAns(words[Math.floor(Math.random()*words.length)]); }
 
 	const handleInput = (userInput) => {
 		if (/^[a-zA-Z]+$/.test(userInput) || userInput === '') {
-			if (userInput.length > 4 && userInput !== '') {
-				setMsg('The word is only 4 letters long');
-			} else {
+			if (userInput.length <= 4 || userInput === '') {
 				setInput(userInput.toUpperCase());
 				setMsg('');
 			}
@@ -28,41 +30,48 @@ const Game = () => {
 				setAttempts(attempts + 1);
 				setSuccess(true);
 				setMsg('Congrats! You guessed correctly');
-			} else if (word.length !== 4) {
-				setMsg('The word must be 4 letters long!');
-			} else {
+				setInputDisabled('disabled');
+			} else if (word.length === 4) {
 				setAttempts(attempts + 1);
 				setMsg(letterChecker(word));
 			}
 		} else {
 			setMsg(word + ' is not a word');
 		}
-		if (attempts >= 4) {
+		if (attempts > 4 && !success) {
 			setFailure(true);
 			setMsg('Oh no, you have failed! The word was ' + wordAns);
+			setInputDisabled('disabled');
+		} else {
+			ref.current.focus();
 		}
 	}
 
 	const letterChecker = (word) => {
 		let matchedLetters = [];
+		let matchedPositions = [];
 		let correctWord = wordAns;
 		let inputWord = word;
-		for (let i = 0; i < correctWord.length; i++) {
-			for (let j = 0; j < inputWord.length; j++) {
-				if (inputWord[j] === correctWord[i] && inputWord.split('').filter(letter => letter === inputWord[j]).length > matchedLetters.filter(letter => letter === inputWord[j]).length) {
-					matchedLetters.push(inputWord[j]);
+		for (let i = 0; i < inputWord.length; i++) {
+			for (let j = 0; j < correctWord.length; j++) {
+				if (inputWord[i] === correctWord[j] && inputWord.split('').filter(letter => letter === inputWord[i]).length > matchedLetters.filter(letter => letter === inputWord[i]).length) {
+					if (i === j) {
+						matchedPositions.push(inputWord[i]);
+					}
+					matchedLetters.push(inputWord[i]);
 					break;
 				}
 			}
 		}
-		let checkedString = matchedLetters.length > 0 ? 'You got ' + matchedLetters.join(', ') + ' correct' : 'You matched no letters correctly';
+		let checkedString = matchedLetters.length > 0 ? 'You got ' + matchedLetters.join(', ') + ' correct.' : 'You matched no letters correctly.';
+		checkedString += matchedPositions.length > 0 ? 'You got ' + matchedPositions.join(', ') + ' in the right position(s).' : '';
 		return checkedString;
 	}
 
 	return (
 		<div id="wumbleGame">
 			<h1>Wumble!</h1>
-			<input id="firstGuess" className="guess" type="text" onChange={e => handleInput(e.target.value)} value={input} />
+			<input autoFocus disabled={inputDisabled} ref={ref} id="firstGuess" className="guess" type="text" onChange={e => handleInput(e.target.value)} value={input} />
 			<div id="msg">{msg}</div>
 			{!success && !failure &&
 				<button id="inputCheck" onClick={() => checkInput(input)}>Check Answer</button>
